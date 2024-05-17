2589273a522846edc1920da989cc063643cd560b
mecastor <mecastor87@gmail.com>
2024-05-17 09:26:44 +0100

    Added jet parse middleware to create userId from mongoose _id using confirmed auth0Id token

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,51 @@
 import { auth } from "express-oauth2-jwt-bearer";
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+import User from "../models/user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
-  audience: process.env.AUTH0_AUDIENCE,
+  audience: process.env.AUTH0_AUDIENCE as string,
   issuerBaseURL: process.env.AUTH0_ISSURE_BASE_URL as string,
   tokenSigningAlg: process.env.AUTH0_TOKEN_SIGNING_ALGO as string,
 });
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || authorization.startsWith("Bearer ")) {
+    return res.sendStatus(401);
+  }
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.decode(token) as JwtPayload;
+    const auth0Id = decoded.sub;
+    const user = await User.findOne({ auth0Id });
+
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
