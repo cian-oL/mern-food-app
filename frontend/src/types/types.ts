5845294eb3154d2b18fce86bc7c8f6559be60693
mecastor <mecastor87@gmail.com>
2024-06-17 17:36:41 +0100

    Created create Restaurant request and associated type

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,3 +1,5 @@
+/* ===== USER TYPES ===== */
+
 export type User = {
   _id: string;
   email: string;
@@ -5,4 +7,23 @@ export type User = {
   addressLine1: string;
   city: string;
   country: string;
+};
+
+/* ===== RESTAURANT TYPES ===== */
+export type Restaurant = {
+  _id: string;
+  user: string;
+  city: string;
+  country: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  imageUrl: string;
+  lastUpdated: string;
+};
+
+export type MenuItem = {
+  name: string;
+  price: number;
 };
