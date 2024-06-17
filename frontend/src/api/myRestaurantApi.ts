b987aff59baae8e0b74f3712baab3031fd2911de
mecastor <mecastor87@gmail.com>
2024-06-17 21:42:10 +0100

    Created useGetMyRestaurant hook & get request

--- frontend/src/api/myRestaurantApi.ts
+++ frontend/src/api/myRestaurantApi.ts
@@ -1,10 +1,42 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 import { Restaurant } from "@/types/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+
+export const useGetMyRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyRestaurantRequest = async (): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching restaurant");
+    }
+    return response.json();
+  };
+
+  const {
+    data: currentRestaurant,
+    isLoading,
+    error,
+  } = useQuery("fetchCurrentRestaurant", getMyRestaurantRequest);
+
+  if (error) {
+    toast.error(error.toString());
+  }
+
+  return { currentRestaurant, isLoading };
+};
 
 export const useCreateMyRestaurant = () => {
   const { getAccessTokenSilently } = useAuth0();
