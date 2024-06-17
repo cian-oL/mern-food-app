72e00d2a94cfac97a434df0e2fd775590d235d4d
mecastor <mecastor87@gmail.com>
2024-06-17 17:46:41 +0100

    Added ImageSection for ManageRestaurantForm

---
+++ frontend/src/forms/manage-restaurant-form/ImageSection.tsx
@@ -0,0 +1,50 @@
+import { useFormContext } from "react-hook-form";
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const ImageSection = () => {
+  const { control } = useFormContext();
+
+  return (
+    <div className="space-y-2">
+      <div>
+        <h2 className="text-xl font-bold">Image</h2>
+        <FormDescription>
+          Add an image that will be displayed on your listing in the search
+          results. Only one image allowed
+        </FormDescription>
+      </div>
+      <div className="flex flex-col gap-8 w-[50%]">
+        <FormField
+          control={control}
+          name="imageFile"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="file"
+                  accept=".jpg, .jpeg, .png, .webp"
+                  onChange={(e) =>
+                    // only one upload allowed
+                    field.onChange(e.target.files ? e.target.files[0] : null)
+                  }
+                  className="bg-white"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ImageSection;
