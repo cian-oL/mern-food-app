83364a8e5eb9d29f240deb7e662facffdb3b381f
mecastor <mecastor87@gmail.com>
2024-08-10 13:10:16 +0100

    Created initial search form with check for isDirty state

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,74 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormField, FormItem } from "./ui/form";
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const formSchema = z.object({
+  searchQuery: z.string({ required_error: "Restaurant name required" }),
+});
+
+export type SearchForm = z.infer<typeof formSchema>;
+
+type Props = {
+  onSubmit: (formData: SearchForm) => void;
+  placeholder: string;
+  onReset?: () => void;
+};
+
+const SearchBar = ({ onSubmit, placeholder, onReset }: Props) => {
+  const form = useForm<SearchForm>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const handleReset = () => {
+    form.reset({
+      searchQuery: "",
+    });
+
+    if (onReset) {
+      onReset();
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Search
+          strokeWidth={2.5}
+          size={30}
+          className="ml-1 text-orange-500 hidden md:block"
+        />
+        <FormField
+          control={form.control}
+          name="searchQuery"
+          render={(field) => (
+            <FormItem className="flex-1">
+              <Input
+                {...field}
+                placeholder={placeholder}
+                className="border-none shadow-none text-xl focus-visible:ring-0"
+              />
+            </FormItem>
+          )}
+        />
+        {form.formState.isDirty && (
+          <Button
+            type="button"
+            onClick={handleReset}
+            variant="outline"
+            className="rounded-full"
+          >
+            Clear
+          </Button>
+        )}
+        <Button type="submit" className="rounded-full bg-orange-500">
+          Search
+        </Button>
+      </form>
+    </Form>
+  );
+};
+export default SearchBar;
